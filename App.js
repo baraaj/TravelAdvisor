ad2b41418d4f147accd7af273896f7e6bd043971
baraaj <jeridibaraa@gmail.com>
2022-11-28 22:36:40 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, Text, View,StyleSheet } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from '../TravelAdvisor/screens/HomeScreen';
+import Discover from './screens/Discover';
+import ItemScreen from './screens/ItemScreen';
+import * as React from 'react';
+import Travel from './screens/Travel';
+import DetailsScreen from './screens/DetailsScreen';
+ 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Travel" component={Travel} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+        <Stack.Screen name="ItemScreen" component={ItemScreen} />
+        <Stack.Screen name="Discover" component={Discover} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </TailwindProvider>
   );
 }
 
---
+++ api/index.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+export const getPlacesData = async (bl_lat, bl_lng, tr_lat, tr_lng, type) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: bl_lat ? bl_lat : '11.847676',
+          tr_latitude: tr_lat ? tr_lat : '108.473209',
+          bl_longitude: bl_lng ? bl_lng : '109.095887',
+          tr_longitude: tr_lng ? tr_lng : '109.149359',
+          restaurant_tagcategory_standalone: '10591',
+           restaurant_tagcategory: '10591',
+           limit: '30',
+           currency: 'USD',
+           open_now: 'false',
+           lunit: 'km',
+          lang: 'en_US'
+        },
+        headers: {
+          'X-RapidAPI-Key': '70924b2af6msh1d9f943d2e4710ap18918fjsn251db49937e5',
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
