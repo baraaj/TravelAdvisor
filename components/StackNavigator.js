c01121b5e21bf598b4d769d47bfd6a65e4bf30b4
baraaj <jeridibaraa@gmail.com>
2022-12-06 18:30:28 +0100

    chat commit

--- components/StackNavigator.js
+++ components/StackNavigator.js
@@ -44,7 +44,7 @@ const StackNavigator = () => {
         <Stack.Screen name="MyProfile" component={MyProfile} />
         <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Travel" component={Travel} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat" component={Chatr} />
         <Stack.Screen name="Share" component={Experience} />
        
         
